dcaa74d13efde0b488752d8068c3901cd318159c
Emilio Betancourt <embeor@gmail.com>
2018-10-09 03:48:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,7 @@ def prime?(integer)
   range = (2...integer).to_a
   if integer < 0 || integer == 0 || integer == 3
     return false
-  else integer > 2
+  else integer 
     range.all? do |number|
       if integer % number != 0
       end
