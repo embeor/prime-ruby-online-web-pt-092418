ad3c00fccc66f072372634bf32d3cc4c9e125f50
Emilio Betancourt <embeor@gmail.com>
2018-10-09 03:40:27 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,16 +1,16 @@
-require "pry"
 def prime?(integer)
   range = (2...integer).to_a
   if integer == 2
-    true
+    return true
   elsif integer > 2
     range.each do |number|
-binding.pry
       if integer % number < 0
         return true
+        true
       end
     end
   else
+    return false
     false
   end
 end
